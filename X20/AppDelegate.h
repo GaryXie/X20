2613827bee41cbfdeda16387db1537c708843827
Gary.Xie <apple@apple-MacB.local>
2012-10-22 23:33:27 +0800

    Initial Commit

---
+++ X20/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  X20
+//
+//  Created by Gary.Xie on 10/22/12.
+//  Copyright (c) 2012 Gary.Xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ X20/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  X20
+//
+//  Created by Gary.Xie on 10/22/12.
+//  Copyright (c) 2012 Gary.Xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
