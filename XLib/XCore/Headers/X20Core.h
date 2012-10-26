aa6143194b0b9ebfdfe5c2e793ed2cb5f407b8de
GaryXie <Workflow@live.cn>
2012-10-26 16:03:26 +0800

    Add X20Core.h

---
+++ XLib/XCore/Headers/X20Core.h
@@ -0,0 +1,14 @@
+//
+//  X20Core.h
+//  X20
+//
+//  Created by Gary Xie on 10/26/12.
+//  Copyright (c) 2012 Gary.Xie. All rights reserved.
+//
+
+#ifndef X20_X20Core_h
+#define X20_X20Core_h
+
+
+
+#endif
