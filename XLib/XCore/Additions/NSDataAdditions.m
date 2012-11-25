97656462f387f2dae23dfab2dd024f2a28e2abd3
Gary.Xie <apple@apple-MacB.local>
2012-11-25 18:37:24 +0800

    Add NSDataAdditions.

--- XLib/XCore/Additions/NSDataAdditions.m
+++ XLib/XCore/Additions/NSDataAdditions.m
@@ -42,114 +42,99 @@ X_FIX_CATEGORY_BUG(NSDataAdditions)
                 ];
 }
 
-@end
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+// base64 code found on http://www.cocoadev.com/index.pl?BaseSixtyFour
+static const char encodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
++(NSData *)dataWithBase64EncodedString:(NSString *)string
+{
+    if(0 == [string length])
+        return [NSData data];
+    
+    static char *decodingTable = NULL;
+    if(NULL == decodingTable)
+    {
+        decodingTable = malloc(256);
+        if(NULL == decodingTable) return nil;
+        memset(decodingTable, CHAR_MAX, 256);
+        NSUInteger i;
+        for(i = 0; i < 64; i++)
+            decodingTable[(short)encodingTable[i]] = i;
+    }
+    
+    const char *characters = [string cStringUsingEncoding:NSASCIIStringEncoding];
+    if(NULL == characters) return nil;
+    char *bytes = malloc((([string length] + 3) / 4) * 3);
+    if(NULL == bytes) return nil;
+    NSUInteger length = 0;
+    
+    NSUInteger i = 0;
+    while (YES) {
+        char buffer[4];
+        short bufferLength;
+        for (bufferLength = 0; bufferLength < 4; i++)
+        {
+            if('\0' == characters[i]) break;
+            if('=' == characters[i] || isspace(characters[i])) continue;
+            buffer[bufferLength] = decodingTable[(short)characters[i]];
+            if(CHAR_MAX == buffer[bufferLength]++)  // Illegal character!
+            {
+                free(bytes);
+                return nil;
+            }
+        }
+        
+        if(0 == bufferLength) break;
+        if(1 == bufferLength)   // At least two characters are needed to produce one byte!
+        {
+            free(bytes);
+            return nil;
+        }
+        
+        // Decode the characters in the buffer to bytes.
+        bytes[length++] = (buffer[0] << 2) | (buffer[1] >> 4);
+        if(bufferLength > 2)
+            bytes[length++] = (buffer[1] << 4) | (buffer[2] >> 2);
+        if(bufferLength > 3)
+            bytes[length++] = (buffer[2] << 6) | buffer[3];
+    }
+    
+    realloc(bytes, length);
+    return [NSData dataWithBytesNoCopy:bytes length:length];
+}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
-//// base64 code found on http://www.cocoadev.com/index.pl?BaseSixtyFour
-//// where the poster released it to public domain
-//// style not exactly congruous with normal three20 style, but kept mostly intact with the original
-//static const char encodingTable[] =
-//"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-//
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-//+ (NSData*)dataWithBase64EncodedString:(NSString *)string {
-//    if ([string length] == 0)
-//        return [NSData data];
-//    
-//    static char *decodingTable = NULL;
-//    if (decodingTable == NULL)
-//    {
-//        decodingTable = malloc(256);
-//        if (decodingTable == NULL)
-//            return nil;
-//        memset(decodingTable, CHAR_MAX, 256);
-//        NSUInteger i;
-//        for (i = 0; i < 64; i++)
-//            decodingTable[(short)encodingTable[i]] = i;
-//    }
-//    
-//    const char *characters = [string cStringUsingEncoding:NSASCIIStringEncoding];
-//    if (characters == NULL)     //  Not an ASCII string!
-//        return nil;
-//    char *bytes = malloc((([string length] + 3) / 4) * 3);
-//    if (bytes == NULL)
-//        return nil;
-//    NSUInteger length = 0;
-//    
-//    NSUInteger i = 0;
-//    while (YES)
-//    {
-//        char buffer[4];
-//        short bufferLength;
-//        for (bufferLength = 0; bufferLength < 4; i++)
-//        {
-//            if (characters[i] == '\0')
-//                break;
-//            if (isspace(characters[i]) || characters[i] == '=')
-//                continue;
-//            buffer[bufferLength] = decodingTable[(short)characters[i]];
-//            if (buffer[bufferLength++] == CHAR_MAX)      //  Illegal character!
-//            {
-//                free(bytes);
-//                return nil;
-//            }
-//        }
-//        
-//        if (bufferLength == 0)
-//            break;
-//        if (bufferLength == 1)      //  At least two characters are needed to produce one byte!
-//        {
-//            free(bytes);
-//            return nil;
-//        }
-//        
-//        //  Decode the characters in the buffer to bytes.
-//        bytes[length++] = (buffer[0] << 2) | (buffer[1] >> 4);
-//        if (bufferLength > 2)
-//            bytes[length++] = (buffer[1] << 4) | (buffer[2] >> 2);
-//        if (bufferLength > 3)
-//            bytes[length++] = (buffer[2] << 6) | buffer[3];
-//    }
-//    
-//    realloc(bytes, length);
-//    return [NSData dataWithBytesNoCopy:bytes length:length];
-//}
-//
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-//- (NSString *)base64Encoding {
-//    if ([self length] == 0)
-//        return @"";
-//    
-//    char *characters = malloc((([self length] + 2) / 3) * 4);
-//    if (characters == NULL)
-//        return nil;
-//    NSUInteger length = 0;
-//    
-//    NSUInteger i = 0;
-//    while (i < [self length])
-//    {
-//        char buffer[3] = {0,0,0};
-//        short bufferLength = 0;
-//        while (bufferLength < 3 && i < [self length])
-//            buffer[bufferLength++] = ((char *)[self bytes])[i++];
-//        
-//        // Encode the bytes in the buffer to four characters,
-//        // including padding "=" characters if necessary.
-//        characters[length++] = encodingTable[(buffer[0] & 0xFC) >> 2];
-//        characters[length++] = encodingTable[((buffer[0] & 0x03) << 4) | ((buffer[1] & 0xF0) >> 4)];
-//        if (bufferLength > 1)
-//            characters[length++] = encodingTable[((buffer[1] & 0x0F) << 2) | ((buffer[2] & 0xC0) >> 6)];
-//        else characters[length++] = '=';
-//        if (bufferLength > 2)
-//            characters[length++] = encodingTable[buffer[2] & 0x3F];
-//        else characters[length++] = '=';
-//    }
-//    
-//    return [[[NSString alloc] initWithBytesNoCopy:characters length:length
-//                                         encoding:NSASCIIStringEncoding freeWhenDone:YES]
-//            autorelease];
-//}
-//// end recycled base64 code
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//@end
+-(NSString *)base64Encoding
+{
+    if(0 == [self length]) return @"";
+    
+    char *characters = malloc((([self length] + 2) / 3) * 4);
+    if(NULL == characters) return nil;
+    NSUInteger length = 0;
+    
+    NSUInteger i = 0;
+    while (i < [self length])
+    {
+        char buffer[3] = {0, 0, 0};
+        short bufferLength = 0;
+        while (bufferLength < 3 && i < [self length])
+            buffer[bufferLength++] = ((char*)[self bytes])[i++];
+        
+        // Encode the bytes in the buffer to four characters, includeing padding '=' characters if necessary.
+        characters[length++] = encodingTable[(buffer[0] & 0xFC) >> 2];
+        characters[length++] = encodingTable[((buffer[0] & 0x03) << 4) | ((buffer[1] & 0xF0) >> 4)];
+        if(bufferLength > 1)
+            characters[length++] = encodingTable[((buffer[1] & 0x0F) << 2) | ((buffer[2] & 0xC0) >> 6)];
+        else characters[length++] = '=';
+        
+        if(bufferLength > 2)
+            characters[length++] = encodingTable[buffer[2] & 0x3F];
+        else characters[length++] = '=';
+    }
+    
+    return [[[NSString alloc] initWithBytesNoCopy:characters length:length
+                                         encoding:NSASCIIStringEncoding freeWhenDone:YES] autorelease];
+}
+
+@end
+
