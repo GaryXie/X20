6808eaeee18f3a8314159a509cf67a760528bb7e
Gary.Xie <apple@apple-MacB.local>
2012-12-25 01:03:54 +0800

    Add XMarkupStripper

--- XLib/XCore/Classes/XMarkupStripper.m
+++ XLib/XCore/Classes/XMarkupStripper.m
@@ -8,6 +8,76 @@
 
 #import "XMarkupStripper.h"
 
+#import "XCorePreprocessorMacros.h"
+#import "XEntityTables.h"
+
 @implementation XMarkupStripper
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)dealloc
+{
+    X_RELEASE_SAFELY(_strings);
+    [super dealloc];
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark NSXMLParserDelegate
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)parser:(NSXMLParser*)parser foundCharacters:(NSString *)string
+{
+    [_strings addObject:string];
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+- (NSData*)parser: (NSXMLParser*)parser
+    resolveExternalEntityName:(NSString *)name systemID:(NSString *)systemID
+{
+    return [[[XEntityTables sharedInstance] iso88591] objectForKey:name];
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#pragma mark -
+#pragma mark Public
+
+-(NSString*)parse:(NSString *)string
+{
+    _strings = [[NSMutableArray alloc] init];
+    
+    NSString* document = [NSString stringWithFormat:@"<x>%@</x>", string];
+    NSData* data = [document dataUsingEncoding:string.fastestEncoding];
+    NSXMLParser* parser = [[NSXMLParser alloc] initWithData:data];
+    parser.delegate = self;
+    [parser parse];
+    X_RELEASE_SAFELY(parser);
+    
+    NSString* result = [_strings componentsJoinedByString:@""];
+    X_RELEASE_SAFELY(_strings);
+    
+    return result;
+    
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//- (NSString*)parse:(NSString*)text {
+//    _strings = [[NSMutableArray alloc] init];
+//    
+//    NSString*     document  = [NSString stringWithFormat:@"<x>%@</x>", text];
+//    NSData*       data      = [document dataUsingEncoding:text.fastestEncoding];
+//    NSXMLParser*  parser    = [[NSXMLParser alloc] initWithData:data];
+//    parser.delegate = self;
+//    [parser parse];
+//    TT_RELEASE_SAFELY(parser);
+//    
+//    NSString* result = [_strings componentsJoinedByString:@""];
+//    TT_RELEASE_SAFELY(_strings);
+//    
+//    return result;
+//}
+
+
 
 @end
